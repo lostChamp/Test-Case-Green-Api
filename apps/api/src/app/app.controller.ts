cf639c4dc66dc197088632284a9129ea555c576d
Kostya <lost.champ2007@gmail.com>
2023-07-28 19:42:35 +0500

    Add Swagger doc

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -3,13 +3,16 @@ import {AmqpConnection} from "@golevelup/nestjs-rabbitmq";
 import {HelloUserDto} from "./dtos/hello-user.dto";
 import {getHelloM1Config} from "@case/rmq-configs";
 import {HelloM1Contract} from "@case/contracts";
+import {ApiBody, ApiOperation, ApiTags} from "@nestjs/swagger";
 
-
+@ApiTags('HelloController')
 @Controller("/helloApp")
 export class AppController {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
   @Post("/byName")
+  @ApiOperation({ summary: 'Send hello for someone' })
+  @ApiBody({ type: HelloUserDto })
   async getHelloByName(@Body() name: HelloUserDto) {
     const helloName = await this.amqpConnection.request<HelloM1Contract.Response>({
       ...getHelloM1Config(),
